969b478b13456d8a32172ee33392539446376c22
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-09-20 22:28:50 +0100

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+
+/home/gracetech/.hushlogin file.
+gracetech@GRACETECH-LENOVO:~$ cd My_Alx_Task/
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task$ cd alx-interview
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview$ mkdir 0x08-making_change
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview$ cd 0x08-making_change
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview/0x08-making_change$ ls
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview/0x08-making_change$ vim README.md
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview/0x08-making_change$ git add README.md
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview/0x08-making_change$ git commit -m "readme content on 0x08. Making Change"
+[main f1ca38b] readme content on 0x08. Making Change
+ 1 file changed, 108 insertions(+)
+ create mode 100644 0x08-making_change/README.md
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview/0x08-making_change$ git ush
+git: 'ush' is not a git command. See 'git --help'.
+
+The most similar command is
+        push
+gracetech@GRACETECH-LENOVO:~/My_Alx_Task/alx-interview/0x08-making_change$ git push
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (3/3), done.
